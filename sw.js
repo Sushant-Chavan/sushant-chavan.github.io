c6ec134fe863e864649d67a736f1ef70d0404514
Sushant Chavan <gitecsvc@gmail.com>
2022-08-08 13:22:33 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-cc74266365b55bdc478e.js"
+    "url": "webpack-runtime-79f1e052976a8d9a42b9.js"
   },
   {
     "url": "framework-7d7b24b52a49e7df0f3b.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c34af341dfa889293c1b61472d874364"
+    "revision": "b29ede2b22e965fbe7a2caf6bba2c64e"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-30d94e0b349286bcc42f.js"
